7827494bda08a985cf3585b289254147e8b3258d
makenaikokoro <854997955@qq.com>
2018-03-11 19:50:01 +0800

    添加迷宫出入口，增加对输入值奇偶的判断

--- AlgoTest/Assets/Scripts/Maze.cs
+++ AlgoTest/Assets/Scripts/Maze.cs
@@ -6,11 +6,19 @@ public class Maze : MonoBehaviour {
     
     public MazeWall wallPrefab;
     public MazeWall[,] walls;
-    public Position mazeSize; // size of maze
+    public Position mazeSize;           // 迷宫大小
+    private  int enterX, enterY;    // 入口坐标
+    private  int endX, endY;              // 出口坐标
 
     public void Init()
     {
-        walls = new MazeWall[mazeSize.x, mazeSize.y];
+        if (mazeSize.x % 2 == 0 || mazeSize.y % 2 == 0)
+        {
+            UnityEditor.EditorUtility.DisplayDialog("错误警告", "输入的数值必须为奇数！", "确定");
+            return;
+        }
+
+        walls = new MazeWall[mazeSize.x, mazeSize.y];          
         for (int x = 0; x < mazeSize.x; x++)
         {
             for (int y = 0; y < mazeSize.y; y++)
@@ -22,6 +30,8 @@ public class Maze : MonoBehaviour {
                 //walls[x, y].IsVisited = false;
             }
         }
+        // 设置出入口
+        FindEnterEndPoint();
     }
 
 	private bool IsWall(int x, int y)
@@ -37,6 +47,22 @@ public class Maze : MonoBehaviour {
         newWall.name = "Maze-" + coordinate.x + "," + coordinate.y;
         newWall.transform.parent = transform;
         newWall.transform.localPosition = new Vector3(coordinate.x, coordinate.y, 0);
+    }
+
+    private void FindEnterEndPoint()
+    {
+        enterX = 0;
+        enterY = mazeSize.y - 2;
+        endX = mazeSize.x - 1;
+        endY = 1;
+        if(walls[enterX, enterY].gameObject != null)
+        {
+            Destroy(walls[enterX, enterY].gameObject);
+        }
+        if(walls[endX, endY].gameObject != null)
+        {
+            Destroy(walls[endX, endY].gameObject);
+        }       
     }
 
     public bool IsInArea(Position coordinate)
